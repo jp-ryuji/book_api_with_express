594b492aee74458208e1e2c7862af754ffd2e5ef
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-03 23:52:32 +0900

    Add api/books route

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const chalk = require('chalk');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const bookRouter = express.Router();
+bookRouter.route('/books')
+  .get(function(req, res) {
+    const responseJson = { hello: 'This is my API' };
+    res.json(responseJson);
+  })
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res) {
   res.send('welcome to my API!');
 });
