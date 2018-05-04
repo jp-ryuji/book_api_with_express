284084d8a8c902f28be4ae9b93aba36f64dea0d2
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-04 20:31:04 +0900

    Implement put

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -27,11 +27,25 @@ const routes = function(Book) {
 
   bookRouter.route('/:bookId')
     .get(function(req, res) {
-      Book.findById(req.params.bookId, function(err, books) {
+      Book.findById(req.params.bookId, function(err, book) {
         if (err) {
           res.status(500).send(err);
         } else {
-          res.json(books);
+          res.json(book);
+        }
+      });
+    })
+    .put(function(req, res) {
+      Book.findById(req.params.bookId, function(err, book) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          book.title = req.body.title;
+          book.author = req.body.author;
+          book.genre = req.body.genre;
+          book.read = req.body.read;
+          book.save();
+          res.json(book);
         }
       });
     });
