020e92ad88ca3163f49d1226b655cf7c9a9b751d
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-04 20:18:16 +0900

    Move the code to routes dir

--- index.js
+++ index.js
@@ -19,39 +19,7 @@ var conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const Book = require('./models/bookModel');
-
-const bookRouter = express.Router();
-bookRouter.route('/books')
-  .post(function(req, res) {
-    let book = new Book(req.body);
-    book.save();
-    res.status(201).send(book);
-  })
-  .get(function(req, res) {
-    let query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, function(err, books) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.json(books);
-      }
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get(function(req, res) {
-    Book.findById(req.params.bookId, function(err, books) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.json(books);
-      }
-    });
-  });
+const bookRouter = require('./routes/bookRoutes')(Book);
 
 app.use('/api', bookRouter);
 
