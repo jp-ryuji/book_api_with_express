45ebf6e8324881a2eee8fa81e2b1998ecaffded5
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-04 21:46:53 +0900

    Refactor to controlllers

---
+++ controllers/bookController.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const bookController = function(Book) {
+  const post = function(req, res) {
+    let book = new Book(req.body);
+    book.save();
+    res.status(201).send(book);
+  }
+
+  const get = function(req, res) {
+    let query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+
+    Book.find(query, function(err, books) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(books);
+      }
+    });
+  }
+
+  return {
+    post: post,
+    get: get
+  }
+}
+
+module.exports = bookController;
